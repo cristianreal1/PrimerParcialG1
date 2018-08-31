67f2ff54d296682e02afa46abca0e301b11bdfec
cristianreal1 <42853932+cristianreal1@users.noreply.github.com>
2018-08-30 20:33:33 -0600

    Tutorial Bootstrap
    Primer Examen Parcial Grupo 1

---
+++ storage/framework/views/864e61360dcb2d0357907527fe9e7737e2b6ba31.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<!-- saved from url=(0030)https://bootswatch.com/flatly/ -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <title>[AYD1]PRIMER PARCIAL</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="<?php echo e(asset('css/bootstrap.css')); ?>" media="screen">
+    <link rel="stylesheet" href="<?php echo e(asset('css/bootstrap.min.css')); ?>">
+  </head>
+  <body style="" class="">
+  <div class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary" style="">
+      <div class="container">
+        <a href="#" class="navbar-brand">[AYD1]PRIMER PARCIAL</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation" style="">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+                  </div>
+                </div>
+              </div>
+          <br />
+          <br />
+          <div class="progress">
+  <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+</div>
+          <br />
+          <br />
+            <div class="container">
+                <main class="py-4">
+
+                <div class="jumbotron">
+  <h1 class="display-3">INSTRUCCIONES</h1>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+</div>
+
+
+<div class="jumbotron">
+  <h1 class="display-3">CONSTRUCCION</h1>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+</div>
+
+
+
+<div class="jumbotron">
+  <h1 class="display-3">CONSTRUCCION</h1>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+</div>
+
+
+
+<div class="jumbotron">
+  <h1 class="display-3">CONSTRUCCION</h1>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+</div>
+
+
+
+<div class="jumbotron">
+  <h1 class="display-3">CONSTRUCCION</h1>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+</div>
+
+
+
+
+<div class="jumbotron">
+  <h1 class="display-3">CONSTRUCCION</h1>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+</div>
+
+
+
+<div class="jumbotron">
+  <h1 class="display-3">CONSTRUCCION</h1>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+</div>
+
+
+
+
+
+
+
+                  </main>
+              </div>
+
+<br />
+<br />
+<br />
+<br />
+</body>
+</html>
